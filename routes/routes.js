eed44cf70d7a49e0699837a0657689fe6b21d485
Pranavprakash30 <prakash.pranav30@gmail.com>
2023-01-12 01:57:28 +0530

    first commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import PostJobs, { deleteJob } from "../components/postJob.js";
+import PostJobs from "../components/postJob.js";
 import { getAllJobs, getJob, searchJobs } from "../components/getjob.js";
 import applyJob from "../components/applyjob.js";
 import getApplicant from "../components/getapplicant.js";
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/postjob", PostJobs);
 router.post("/applyjob/:id", applyJob);
 router.get("/getjob", getJob);
-router.get("/deletejob", deleteJob);
+// router.get("/deletejob", deleteJob);
 router.get("/getalljobs", getAllJobs);
 router.get("/getajobs/search", searchJobs);
 router.get("/getapplicants/:id", getApplicant);
